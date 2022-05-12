195b969c19253c300af25c94e3ec4a9671517d31
Cathy Dao <cathydao95@gmail.com>
2022-05-12 12:34:11 -0700

    commit to main

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,35 @@
+import { useState } from "react";
 import memesData from "../memesData";
 
 function Form(props) {
-  function getMeme() {
-    const retrievedMeme = memesData.data.memes;
-    const randNum = Math.floor(Math.random() * retrievedMeme.length);
-    const url = retrievedMeme[randNum].url;
-    console.log(url);
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+  //   const [memeImage, setMemeImage] = useState("");
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => {
+      return { ...prevMeme, randomImage: url };
+    });
   }
   return (
     <main>
       <div className="form-container">
         <input className="input-1" type="text" />
         <input className="input-2" type="text" />
-        <button onClick={getMeme} className="btn-get">
+        <button onClick={getMemeImage} className="btn-get">
           Get a new meme image
         </button>
       </div>
+      <img src={meme.randomImage} className="meme-img" />
     </main>
   );
 }
